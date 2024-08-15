347ab2e415dcfd9d8a91a093b3871befb1bf0686
kpucko16 <christianchrastev@yahoo.com>
2024-08-15 11:07:36 +0000

    resolving variables issue

--- main.tf
+++ main.tf
@@ -3,46 +3,46 @@ data "aws_vpc" "main" {
 }
 
 resource "aws_security_group" "sg_my_test" {
-  name = "sg_my_test"
+  name        = "sg_my_test"
   description = "My test security group"
-  vpc_id = data.aws_vpc.main.id
+  vpc_id      = data.aws_vpc.main.id
 
   ingress = [
     {
-        description = "HTTP"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = false
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     },
     {
-        description = "SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip_with_cidr]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = false
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = [var.my_ip_with_cidr]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 
   egress = [
     {
-        description = "The default egress rule"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-        prefix_list_ids = []
-        security_groups = []
-        self = false
+      description      = "The default egress rule"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 }
@@ -57,29 +57,29 @@ resource "aws_key_pair" "deployer" {
 }
 
 data "template_file" "user_data" {
-    template = file("${abspath(path.module)}/userdata.yaml")
+  template = file("${abspath(path.module)}/userdata.yaml")
 }
 
 data "aws_ami" "amazon-linux-2" {
-    provider = aws
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name   = "owner-alias"
-        values = ["amazon"]
-    }
-    filter {
-        name   = "name"
-        values = ["amzn2-ami-hvm*"]
-    }
+  provider    = aws
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type = var.instance_type 
-  key_name = "${aws_key_pair.deployer.key_name}"
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.sg_my_test.id]
-  user_data = data.template_file.user_data.rendered
+  user_data              = data.template_file.user_data.rendered
 
   tags = {
     Name = var.server_name
--- variables.tf
+++ variables.tf
@@ -1,32 +1,33 @@
 variable "my_ip_with_cidr" {
-    type = string
-    description = "Provide your IP eg. 213"
-    default = "18.153.33.23/32"
+  type        = string
+  description = "Provide your IP eg. 213"
+  default     = "18.153.33.23/32"
 }
 
-variable "bucket_name"{
-  type = string
+variable "bucket_name" {
+  type    = string
   default = "nakriskopraznatakofa123"
+}
 
 variable "public_key" {
-  type = string
+  type    = string
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC6dMcVfumL7NECaA1ZOe06lyuLfYJNXPFZh6W5zEbOGrfhJ39+JpzRERbh/v1l0VA+IYNg0wQ/MsJ82k9LfH/qCToZLxclDDwIuSgeVlKhJHnAHEOI6+ZlRPhuPVDJ1zX3CUc5AP7KtF8dg2jf8ulnCvunF/n62xa1+iDMhfBTWx//gamreTRmjoWJ5Itwiwx6oCmp1W0p8Jh/RmleyiRB3/d6HDwe8REFw6aJW8qahI651SlEwqLaEc4owg4pXx/sPtthikMxFmpS55dIXg8tWYm2webtfhD438o0UmH5Nlnng4JmCIIQTcjOwXHG2n7DejGHRCNnkxNKppyAYg0c34uUgUDs+XqC/MfwrxcEim+xbIa8sncv9CnkmCtGhv0SsTyWhk2CmJYbAle2mY5MLQIULmv1cP8IpZRDn0CLAEquu5Q8/AUOSMwpEstlhA+D4QrZrtmOIPUkNozazmS+dIilx+I7UZU2ZXj2hukMEV0HMQLMmpIXDE5HBV5cSl3eaHMOU1nkvmzHkahWyeJmwaqhFsgnmYDeWG8LTHoCCYfwwSJWbHlUw9BA9DFFDat7k+ma0cDYnCBeRc1iDL0rXUFIbO9GEDBVITix7lD+6ZDcTHJCBw0MMnUWbdspo/R2fW0oWhFVQaO6dpPQdiYTLcNb+28QGyl95mxnBIUZZw== christianchrastev@yahoo.com"
 }
 
 variable "instance_type" {
-    type = string
-    description = "Please input the instance type you would like to deply." 
-    sensitive = true
-    default = "t2.micro"
+  type        = string
+  description = "Please input the instance type you would like to deply."
+  sensitive   = true
+  default     = "t2.micro"
 }
 
 variable "server_name" {
-  type = string
+  type        = string
   description = "Please input the name of your server"
-  default = "My-test-server"
+  default     = "My-test-server"
 }
 
 variable "vpc_id" {
-  type = string
+  type    = string
   default = "vpc-0902283be7140841f"
 }
